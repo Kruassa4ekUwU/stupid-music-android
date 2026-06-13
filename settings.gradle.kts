c96cacdb100ff367b7c2bb27a59411b6bcff4899
Kruassa4ekUwU <kruassa4ek@gmail.com>
2026-06-14 02:23:59 +0300

    fix: NewPipe extractor

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // Нужен для NewPipe Extractor
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "StupidMusic"
